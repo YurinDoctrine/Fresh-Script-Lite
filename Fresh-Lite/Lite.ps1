9675fbd5eb63475679c22e8a8377068e57005b29
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2021-07-25 23:30:31 +0300

    Edit Fresh-Lite/Lite.ps1

--- Fresh-Lite/Lite.ps1
+++ Fresh-Lite/Lite.ps1
@@ -29,13 +29,8 @@ function Check {
 Check
 #region WinGet
 function WinGet {
-	# $WinGetURL = Invoke-RestMethod -Uri "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
-	# Start-BitsTransfer $WinGetURL.assets.browser_download_url
 	# TODO check if its works either not
-	# Install WinGet.
-	# Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
-	winget install -e --id Microsoft.dotNetFramework | Out-Host
-	winget install -e --id Notepad++.Notepad++ | Out-Host
+	$WinGetURL = Invoke-RestMethod -Uri "https://api.github.com/repos/microsoft/winget-cli/releases/latest"; Start-BitsTransfer $WinGetURL.assets.browser_download_url; Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle; winget install -e --id Microsoft.dotNetFramework | Out-Host; winget install -e --id Notepad++.Notepad++ | Out-Host
 }
 WinGet
 #endregion WinGet
