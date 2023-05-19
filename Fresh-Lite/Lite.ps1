b378e94a5180a15f4d5d7a1cfa394fb80d57111c
Yurin Doctrine <doctriney@protonmail.com>
2023-05-19 12:45:19 +0300

    edit Fresh-Lite/Lite.ps1

--- Fresh-Lite/Lite.ps1
+++ Fresh-Lite/Lite.ps1
@@ -611,6 +611,7 @@ function Performance {
     auditpol /set /category:"System" /success:disable
     auditpol /set /category:"System" /failure:disable
 
+    Set-MpPreference -QuickScanScheduleEnabled $True
     Set-MpPreference -ScanAvgCPULoadFactor 5
     Set-MpPreference -EnableLowCpuPriority $True
     Set-MpPreference -ScanOnlyIfIdleEnabled $True
